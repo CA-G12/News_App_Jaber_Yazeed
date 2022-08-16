dfcedcbd627d7f40a46ee19f55f558754287681c
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-08-16 20:20:48 +0300

    Created server in  app.js Relates #3

--- src/app.js
+++ src/app.js
@@ -7,12 +7,16 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use(path.join(__dirname, '..', 'public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "..", 'public', 'errors', '404.html'));
 })
 
-app.use((err,req, res, next) => {
+app.use((err, req, res, next) => {
     res.status(500).sendFile(path.join(__dirname, "..", 'public', 'errors', '500.html'));
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Listening to port ${app.get('port')}`);
 })
